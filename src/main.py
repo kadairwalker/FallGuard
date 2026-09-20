53ed3c57b80881d7164b145fb774ecdfcba08fd4
Dharma Swaroop <124238434+DharmaSwaroop@users.noreply.github.com>
2026-09-19 21:00:31 -0400

    Update main.py
    Cleaning up main.py

--- src/main.py
+++ src/main.py
@@ -8,9 +8,6 @@ SAMPLE_RATE_HZ = 50
 # False keeps using fake data, for continued testing.
 USE_REAL_SENSOR = True
 
-if USE_REAL_SENSOR:
-    from hardware.i2c_reader import init_accelerometer, read_accelerometer
-
 
 if USE_REAL_SENSOR:
     from hardware.i2c_reader import init_accelerometer, read_accelerometer
